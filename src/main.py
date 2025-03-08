cb6954a0a414f2057c8a53c0cd61ebcad466f532
luecha.kan <luecha.kanm@gmail.com>
2025-03-08 17:23:58 +0700

    Query Parameters

--- src/main.py
+++ src/main.py
@@ -20,9 +20,28 @@ BOOK = [
 async def read_books():
     return BOOK
 
+
 @app.get("/books/{bool_title}")
 async def read_books(bool_title: str):
     for book in BOOK:
         if book.get('title').casefold() == bool_title.casefold():
             return book
-    
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOK:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOK:
+        if book.get('author').casefold() == book_author.casefold() \
+            and book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
