a6d28a1b6edb90522b0139068b6c9957713f62f5
luecha.kan <luecha.kanm@gmail.com>
2025-03-09 18:06:06 +0700

    Passing Path & Query Parameters To Request

--- src/main.py
+++ src/main.py
@@ -8,12 +8,25 @@ class Profile(BaseModel):
     age: int
 
 
+class Product(BaseModel):
+    name: str
+    price: int
+    discount: int
+    discounted_price: float
+
+
 app = FastAPI()
 
 
 @app.get("/user/admin")
 async def get_admin():
     return {"This is admin page"}
+
+
+@app.post("/product/create")
+async def create_product(product: Product, product_id: int, category: str):
+    product.discounted_price = product.price - (product.price * product.discount) / 100
+    return {'product_id': product_id, 'product': product, 'category': category}
 
 
 @app.post("/user/create")
